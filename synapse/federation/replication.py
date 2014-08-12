9ef41e6c3b6494e81cf31bb3a7eea950e630c932
Mark Haines <mark.haines@openmarket.com>
2014-08-12 12:06:50 +0100

    move coding to top of file since it needs to be in first two lines

--- synapse/federation/replication.py
+++ synapse/federation/replication.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2014 matrix.org
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# -*- coding: utf-8 -*-
 """This layer is responsible for replicating with remote home servers using
 a given transport.
 """
