1afbb23a82f0f714b96bc0140fbf606a2d9b4135
Amber H. Brown <hawkowl@atleastfornow.net>
2020-02-26 23:35:23 +1100

    update nix

--- docker/synapse-deps.nix
+++ docker/synapse-deps.nix
@@ -1,3 +1,3 @@
 with import <nixpkgs> {};
 
-python37.withPackages (ps: with ps; [ phonenumbers canonicaljson pymacaroons psycopg2 psutil parameterized pillow lxml pysaml2 bleach service-identity six frozendict pip idna pyasn1-modules netaddr jsonschema pynacl signedjson pyyaml pyopenssl mock bcrypt jinja2 msgpack attrs twisted pyasn1 treq sortedcontainers daemonize unpaddedbase64 ])
+python37.withPackages (ps: with ps; [ phonenumbers canonicaljson pymacaroons psycopg2 psutil parameterized pillow lxml pysaml2 bleach service-identity six frozendict pip idna pyasn1-modules netaddr jsonschema pynacl pyyaml pyopenssl mock bcrypt jinja2 msgpack attrs twisted pyasn1 treq sortedcontainers daemonize unpaddedbase64 ])
